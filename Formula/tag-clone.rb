c3ff8d908f146cce5bc8570a3e0fdeeb687436d3
Matt Chan <matthew.chan@microsoft.com>
2026-01-06 23:42:34 -0800

    update formula

--- Formula/tag-clone.rb
+++ Formula/tag-clone.rb
@@ -1,7 +1,7 @@
 class TagClone < Formula
   desc "Copy macOS file tags from JPG files to corresponding ARW files"
-  homepage "https://github.com/yourusername/tag-clone"
-  version "1.0.0"
+  homepage "https://github.com/matt-chan/tag-clone"
+  version "0.0.1"
   license "MIT"
 
   # For local development, use a file URL or update with your actual repo
@@ -9,7 +9,7 @@ class TagClone < Formula
   # sha256 "YOUR_SHA256_HERE"
   
   # For local installation during development:
-  url "file://#{HOMEBREW_LIBRARY}/Taps/yourusername/homebrew-tools/tag-clone-1.0.0.tar.gz"
+  url "file://#{HOMEBREW_LIBRARY}/Taps/matt-chan/homebrew-tools/tag-clone-0.0.1.tar.gz"
   sha256 "placeholder"
 
   depends_on "tag"
